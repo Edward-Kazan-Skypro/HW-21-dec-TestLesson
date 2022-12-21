48882f65c7573660add1a9309cef73d0600d8931
Edward-Kazan-Skypro <zvezda_torgi@mail.ru>
2022-12-21 15:12:50 +0300

    Домашнее задание от 21 декабря - тестирование

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -3,6 +3,25 @@ package org.example;
 
 public class User {
     private String login;
-    private String eMail;
+    private String Email;
 
+    public User(String login, String Email) {
+        this.login = login;
+        this.Email = Email;
+    }
+
+    public User() {
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public boolean checkEmail(String Email) {
+        return Email.contains("@") & Email.contains(".");
+    }
 }
